99d88914befaff1f6f1175007dd816def332c084
Fatima Ezzahra <felemenoun@gmail.com>
2021-12-13 15:58:17 +0100

    add menu

---
+++ script.js
@@ -0,0 +1,16 @@
+let isVisible = false;
+let test = true;
+
+let getnav = document.querySelector(".mobile_menu");
+ 
+   
+
+
+ let mobileMenu = function() {
+   getnav.style.visibility = 'visible';
+    
+ }
+
+ let closeMenu = function () {
+    getnav.style.visibility = 'hidden';
+ }
