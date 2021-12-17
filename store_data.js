13fcf6649297e54ac8cebddcef65832f85379fd3
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-17 22:18:28 +0500

    Update local storage for Portfolio

--- store_data.js
+++ store_data.js
@@ -1,9 +1,4 @@
 const form2 = document.querySelector('#submit_form');
-// const fullName = form2.elements[0];
-// const emailAdress = form2.elements[1];
-// const msg = form2.elements[2];
-
-/// /// Store data in object
 
 form2.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -16,27 +11,12 @@ form2.addEventListener('submit', (e) => {
   localStorage.setItem('DATA', JSON.stringify(obj));
 });
 
-/// // store data in locale storage
+let getData = localStorage.getItem('DATA');
+let get_data = JSON.parse(getData);
 
-// let name = document.querySelectorAll('.input1');
-
-// localStorage.setItem('DATA', JSON.stringify(dataForm));
-
-// name.forEach( input => {
-//     input.addEventListener('change',() => {
-
-//     })
-// })
-
-/// //// Get data
-
-// let getData = localStorage.getItem('DATA');
-
-// let get_data = JSON.parse(getData);
-
-// form2.addEventListener('load', (e) => {
-//     e.target.value = get_data.fullname;
-//     e.target.value = get_data.EmailAdress;
-//     e.target.value = get_data.message;
-// }
-// );
+form2.addEventListener('load', (e) => {
+    fullname.value = get_data.fullname;
+    EmailAdress.value = get_data.EmailAdress;
+    message.value = get_data.message; 
+}
+);
